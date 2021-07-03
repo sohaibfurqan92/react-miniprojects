4371a3e134dcd943461ffa99a45f8f1d115b1b1a
Sohaib Furqan <sohaibfurqan@hotmail.com>
2021-07-03 15:54:24 +0500

    Completed accordion project

--- 04-accordion/src/App.js
+++ 04-accordion/src/App.js
@@ -1,5 +1,20 @@
+import data from './data';
+import Question from './Question';
+
 function App() {
-  return <h1>Accordion Project Starter</h1>;
+  return (
+    <>
+      <div className='container'>
+        <h1>Questions and answers about Login</h1>
+        <div className='questions'>
+          {data.map((question) => {
+            console.log(question.title);
+            return <Question key={question.id} {...question} />;
+          })}
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;
